2aebdb8dcbfb2d236bebad5aa3a56fc2a25a4566
snehesht <mail@snehesh.me>
2016-02-22 16:41:22 -0500

    added fish shell configs

---
+++ fish-confg/fish/config.fish
@@ -0,0 +1,9 @@
+#function fish_prompt
+#    /home/warlock/powerline-shell.py $status --shell bare ^/dev/null
+#end
+
+#set fish_function_path $fish_function_path "/usr/lib/python3.5/site-packages/powerline/bindings/fish" 
+#powerline-setup
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
