61152bc3dff27d3f44b0b9ea9961ee28ad6df0c0
snehesht <mail@snehesh.me>
2016-10-14 10:22:14 -0400

    pomodoro zsh plugin

--- zsh/functions/pomodoro.zsh
+++ zsh/functions/pomodoro.zsh
@@ -1,7 +1,9 @@
+# Install Sox
 func pomodoro(){
 while true
 	do
 		sleep $1
-		play --no-show-progress --null --channels 1 synth .3 sine 250
+		play --no-show-progress ~/dotfiles/zsh/bell_multi.wav
+		#play --no-show-progress --null --channels 1 synth .3 sine 250
 done
 }
